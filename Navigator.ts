b5974f606905c587913c83164bd7bc5b7f3c5977
Brian Johnson <brian.leroy.johnson@gmail.com>
2018-11-20 16:41:03 -0500

    Add auth stack and protected app stack to navigator.

--- Navigator.ts
+++ Navigator.ts
@@ -1,13 +1,18 @@
-import { createStackNavigator } from 'react-navigation';
+import { createAppContainer, createStackNavigator, createSwitchNavigator } from 'react-navigation';
 import { HomeContainer } from './Home';
 import { Loading } from './Loading';
 import { LogInContainer } from './LogIn';
 
-export const Navigator = createStackNavigator({
-  Home: { screen: HomeContainer },
-  Loading: { screen: Loading},
-  LogIn: { screen: LogInContainer }
-},
-{
-  initialRouteName: 'Loading'
-});
+const AppStack = createStackNavigator({ Home: HomeContainer });
+const AuthStack = createStackNavigator({ LogIn: LogInContainer });
+
+export const Navigator = createAppContainer(createSwitchNavigator(
+  {
+    App: AppStack,
+    Auth: AuthStack,
+    AuthLoading: Loading,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
+));
